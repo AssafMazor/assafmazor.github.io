40bc14a2562a2e07d6280a1c3c56d8cf785a1df4
Assaf Mazor <assaf.player.king@gmail.com>
2021-12-21 10:16:11 +0200

    ....

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-import {UI_Main} from './components/main/mainComponets/mainComponets.js';
-import {DataService} from './services/Data.Service.js';
-
-window.app = {};
-
-$(document).ready(function (){
-    app.dataService = new DataService();
-    new UI_Main();
+$(document).ready(function () {
+    $(window.scroll).scroll(function () {
+        if(window.pageYOffset > 100){
+            $(".arrow-up-wrap").removeClass("hide")
+        }else {
+            $(".arrow-up-wrap").addClass("hide")
+        }
+    })
 })
+function onScrollDownBtnClick(){
+    $("html, body").animate({
+        scrollTop: $(".projects-page").offset().top - 10
+    }, "slow");
+    console.log($("body").offset().top)
+}
+
